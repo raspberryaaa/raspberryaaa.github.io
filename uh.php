a9f62f4f8a0931cd49771a2cb71d4bd1bc1af251
raspberryaaa <85763523+raspberryaaa@users.noreply.github.com>
2021-08-17 02:07:32 +0200

    Add files via upload

---
+++ uh.php
@@ -0,0 +1,15 @@
+ <!DOCTYPE html>
+ <style>
+ body {
+	background-color: #6edc8e;
+	font-family: "Comic Sans MS;
+	font-size: 400px
+ <style>
+ <html>
+ <body>
+ <?php
+echo(rand(1, 100));
+?> 
+<body>
+</html>
+
